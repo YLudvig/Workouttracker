ff2db78e671e47a5ad226e633ca50bb076858a86
YLudvig <yslu24ne@student.ju.se>
2025-10-20 11:18:23 +0200

    Start

---
+++ src/test/java/com/workouttracker/workouttracker/WorkouttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workouttracker.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkouttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
